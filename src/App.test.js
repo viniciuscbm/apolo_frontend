7887be3bfefcd2e0ea19e7bf359d8ca91093ef84
Vinicius Camboim <vini.camboim9@gmail.com>
2020-11-23 00:43:02 -0300

    setup testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('renders learn react link', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('Learn React')).toBeInTheDocument()
+})
