7fac81056f62d5f85d6afed14c20e57781b524ba
Vinicius Camboim <vini.camboim9@gmail.com>
2020-11-24 01:05:41 -0300

    cleans the project and adds material ui

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders learn react link', () => {
+it('renders click me button', () => {
   const { getByText } = render(<App />);
-  expect(getByText('Learn React')).toBeInTheDocument();
+  expect(getByText('click me')).toBeInTheDocument();
 });
